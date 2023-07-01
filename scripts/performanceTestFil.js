cb93e4a6ca41835ee92cb1c25216f685667aac90
Alison Haire <alihaire900@gmail.com>
2023-07-01 16:47:51 +1000

    benchmarking complete

--- scripts/performanceTestFil.js
+++ scripts/performanceTestFil.js
@@ -6,13 +6,22 @@ async function main() {
   const network = await hre.network.name;
   const fileName = 'performance-results-' + network + '.json';
 
+  const owner = new ethers.Wallet(
+    process.env.WALLET_PRIVATE_KEY,
+    ethers.provider
+  );
+
   const results = [];
   const executionTimes = [];
   const gasUsages = [];
 
   // Deploy the contract and measure deployment gas used
   const startDeployTimestamp = Date.now();
-  const contract = await ethers.deployContract('Greeter');
+  const contract = await ethers.deployContract('Greeter', {
+    signer: owner,
+    gasLimit: 5000000,
+    gasPrice: 1000000000,
+  });
   await contract.waitForDeployment();
   // const { gasUsed: deployGasUsed } = await contract.deployTransaction.wait(); // Wait for deployment confirmation and get gas used
   const endDeployTimestamp = Date.now();
@@ -27,13 +36,22 @@ async function main() {
   for (let i = 0; i < iterations; i++) {
     const startTimestamp = Date.now();
     // const txRes = await contract.performComputations(10); // Replace with your function call
-    const txRes = await contract.setGreeting(network.name);
+    console.log(network);
+    const txRes = await contract.setGreeting(network, {
+      gasLimit: 5000000,
+      gasPrice: 1000000000,
+    });
     console.log(`Transaction result: ${txRes}`);
     // await txRes.wait(); // Wait for transaction confirmation
     const txReceipt = await txRes.wait(); // Wait for transaction confirmation and get gas used
+    console.log(txReceipt);
     const gasUsed = txReceipt.gasUsed;
-    const greet = await contract.greet();
-    console.log(greet.toString());
+    try {
+      const greet = await contract.greet();
+      console.log(greet.toString());
+    } catch (error) {
+      console.error('Error calling greet:', error);
+    }
     const endTimestamp = Date.now();
     const executionTime = endTimestamp - startTimestamp;
     console.log(`Execution time for run ${i + 1}: ${executionTime} ms`);
@@ -51,7 +69,7 @@ async function main() {
   // Calculate average time and average gas usage over number of iterations
   const averageTime =
     executionTimes.reduce((acc, time) => acc + time, 0) / iterations;
-  const averageGas = gasUsages.reduce((acc, gas) => acc + gas, 0) / iterations; //this is wrong
+  const averageGas = gasUsages.reduce((acc, gas) => acc + gas, 0) / iterations;
   console.log(`Average execution time: ${averageTime} ms`);
   console.log(`Average gas used: ${averageGas}`);
 
